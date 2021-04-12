d6812e6c1d462c1275a0d4fba27bad10f058f618
Ryan Dill <ryand1127@gmail.com>
2021-04-12 16:08:02 -0400

    finished mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, SET_MEMORY, APPLY_MEMORY, RESET_MEMORY } from './../actions';
 
 export const initialState = {
-    total: 100,
-    operation: "*",
-    memory: 100
+    total: 0,
+    operation: "+",
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -14,6 +14,8 @@ const calculateResult = (num1, num2, operation) => {
             return num1 * num2;
         case("-"):
             return num1 - num2;
+        default:
+            return 'error'
     }
 }
 
@@ -30,13 +32,31 @@ const reducer = (state, action) => {
                 ...state, 
                 total: calculateResult(state.total, action.payload, state.operation)
             });
-        
         case(CHANGE_OPERATION):
             return ({
                 ...state,
                 operation: action.payload
             });
-            
+        case(CLEAR_DISPLAY):
+            return ({
+                ...state,
+                total: state.total - state.total
+            });
+        case(SET_MEMORY):
+            return ({
+                ...state,
+                memory: state.total
+            })
+        case(APPLY_MEMORY):
+            return ({
+                ...state,
+                total: calculateResult(state.total, state.memory, state.operation)
+            })
+        case(RESET_MEMORY):
+            return ({
+                ...state,
+                memory: state.memory - state.memory
+            })
         default:
             return state;
     }
